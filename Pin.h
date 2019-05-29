51c2fd1ec0b009e91e27cd3acdacf39597df2d91
Maxim Kononenko <kononenko@gmail.com>
2019-05-29 20:29:11 +0300

    Constructor without arguments

--- Pin.h
+++ Pin.h
@@ -5,8 +5,8 @@ class Pin
     uint32_t pin;
 
 public:
-    Pin(uint32_t _pin) : pin(_pin) {}
-    void setup(uint32_t mode = OUTPUT) { pinMode(pin, mode); }
+    Pin() {}
+    void setup(uint32_t _pin, uint32_t mode = OUTPUT) { pin = _pin; pinMode(_pin, mode); }
     void set(uint32_t level) { digitalWrite(pin, level); }
     uint32_t get() { return digitalRead(pin); }
     void on() { set(HIGH); }
