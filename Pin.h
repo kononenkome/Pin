aef54c81e58684125c0ea5f2620487be5faab7c3
Maxim Kononenko <kononenko@MacBook-Pro-Maxim.local>
2019-06-15 12:29:28 +0300

    constructor with parameters

--- Pin.h
+++ Pin.h
@@ -2,13 +2,24 @@
 
 class Pin
 {
-    uint32_t pin;
+    int pin = -1;
+    int mode = OUTPUT;
+    bool ready = false;
 
 public:
     Pin() {}
-    void setup(uint32_t _pin, uint32_t mode = OUTPUT) { pin = _pin; pinMode(_pin, mode); }
-    void set(uint32_t level) { digitalWrite(pin, level); }
-    uint32_t get() { return digitalRead(pin); }
+    Pin(int _pin, int _mode = OUTPUT) : pin(_pin), mode(_mode) {};
+    void setup(int _pin, int _mode = OUTPUT) { 
+        if (!ready) { 
+            pin = _pin; 
+            mode = _mode;
+            pinMode(_pin, mode); 
+            ready = true); 
+            }
+    }
+    void reset() { ready = false; }
+    void set(int level) { digitalWrite(pin, level); }
+    int get() { return digitalRead(pin); }
     void on() { set(HIGH); }
     void off() { set(LOW); }
     void toggle() { set(!get()); }
